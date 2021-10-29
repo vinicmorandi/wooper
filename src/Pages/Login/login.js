6eb46b0763906bf02a6e80524c7cb580e75c17c9
vinicmorandi <viniciuscmorandi@gmail.com>
2021-10-29 08:49:06 -0300

    ajustei a navbar

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -1,18 +1,40 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 import "./login.css"
 
 const Login = () => {
+
+    const [values, setValues] = React.useState({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+    });
+    
+    const handleClickShowPassword = () => {
+      setValues({
+        ...values,
+        showPassword: !values.showPassword,
+      });
+    };
+
     return (
-        <>
-            <form>
-                <p>Login</p>
-                <div><input type='text' placeholder='Nome'/></div>
-                <div><input type='password' placeholder='Senha'/></div>
-                <div><Button color="secondary" variant="contained" endIcon={<SendIcon />}>Login</Button></div>
-            </form>
-        </>
+        <Box component="form" sx={{'& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
+            <p>Login</p>
+            <TextField id="outlined-basic" label="Email" variant="outlined" />
+            <TextField id="outlined-basic" label="Senha" type='password' onClick={handleClickShowPassword} variant="outlined" />
+            <FormGroup>
+                <FormControlLabel control={<Switch defaultChecked />} label="Lembrar senha" />
+            </FormGroup>
+            <div><Button color="primary" variant="contained" endIcon={<SendIcon />}>Login</Button></div>
+        </Box>
     )
 }
 
--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,40 +1,173 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import { styled, useTheme } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
+import Box from '@material-ui/core/Box';
+import { CssBaseline } from "@material-ui/core";
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
 import Home from "../Pages/Home/home"
 import Login from "../Pages/Login/login"
+import Drawer from '@mui/material/Drawer';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { AccountCircle, Home as HomeIcon } from "@material-ui/icons";
 import "../Assets/css/navbar.css"
 
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(9)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+const Appbar2 = styled(AppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer2 = styled(Drawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+);
+
+
 export default function Routes() {
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+
     return (
         <Router>
             {/* Navbar */}
-            <AppBar position="static">
-            <Toolbar variant="dense">
-                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" color="inherit" component="div">
-                    <Link to="Login">Login</Link>
-                </Typography>
-            </Toolbar>
-            </AppBar>
-            <div className='principal'>
-                {/* Rotas */}
-                <Switch>
-                    <Route path='/login'>
-                        <Login></Login>
-                    </Route>
-                    <Route path='/'>
-                        <Home></Home>
-                    </Route>
-                </Switch>
-            </div>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <Appbar2 position="fixed" open={open}>
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={handleDrawerOpen}
+                            edge="start"
+                            sx={{
+                                marginRight: '36px',
+                                ...(open && { display: 'none' }),
+                            }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" noWrap component="div">
+                            <NavLink activeClassName="ativo" to="/">Home</NavLink>
+                            <NavLink activeClassName="ativo" to="/Pokedex">Pokedex</NavLink>
+                            <NavLink activeClassName="ativo" to="/Login">Login</NavLink>
+                            <NavLink activeClassName="ativo" to="/Login">Login</NavLink>
+                        </Typography>
+                    </Toolbar>
+                </Appbar2>
+                <Drawer2 variant="permanent" open={open}>
+                    <DrawerHeader>
+                        <IconButton onClick={handleDrawerClose}>
+                            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                        </IconButton>
+                    </DrawerHeader>
+                    <Divider />
+                    <List>
+                        <ListItem button key="Home">
+                            <ListItemIcon>
+                                <HomeIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Home" />
+                        </ListItem>
+                    </List>
+                    <Divider />
+                    <List>
+                        <ListItem button key="Login">
+                            <ListItemIcon>
+                                <AccountCircle />
+                            </ListItemIcon>
+                            <ListItemText primary="Login" />
+                        </ListItem>
+                    </List>
+                </Drawer2>
+                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                    <DrawerHeader />
+                    {/* Rotas */}
+                    <Switch>
+                        <Route path='/login'>
+                            <Login></Login>
+                        </Route>
+                        <Route path='/'>
+                            <Home></Home>
+                        </Route>
+                    </Switch>
+                </Box>
+            </Box>
         </Router>
     );
+
 }
