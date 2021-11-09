85d40261d16304e3f62ac149e0af0da42922f203
vinicmorandi <viniciuscmorandi@gmail.com>
2021-11-08 21:04:22 -0300

    mudei um pouco os cards e a home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,13 +2,13 @@
 import React, { useEffect } from "react";
 
 // Material UI
-import { Typography, Card, CardContent, CardActions, CardMedia, Button } from "@mui/material";
+import { Typography, Card, CardContent, CardActions, CardMedia, Button, CardActionArea } from "@mui/material";
 
 // CSS
 import './home.css'
 
 const Home = () => {
-    useEffect(()=>{
+    useEffect(() => {
         document.title = 'Home | Sussy'
     })
 
@@ -20,45 +20,48 @@ const Home = () => {
                 <Typography variant='h6'>Lorem ipsum dolor sit amet</Typography>
             </div>
             {/* Cards */}
-            <div id='divCards'>
-                <Card sx={{ maxWidth: 400 }}>
-                    <CardMedia component="img" height="200" image="https://i.pinimg.com/originals/89/49/e5/8949e5e697ca5d75b045080343c2c8c1.jpg" />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">Batalhas</Typography>
-                        <Typography variant="body2" color="text.secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus maximus neque ut ipsum luctus facilisis. Quisque in facilisis augue. Morbi quis velit et dolor blandit condimentum.</Typography>
-                    </CardContent>
+            <div class='divCards'>
+                <Card sx={{ maxWidth: 520 }}>
+                    <CardActionArea>
+                        <CardMedia component="img" height="550" image="https://i.pinimg.com/736x/f2/c6/3f/f2c63f9746f7b3dec71fce0235226ac7.jpg" alt="green iguana" />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">Lizard</Typography>
+                            <Typography variant="body2" color="text.secondary">Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
                     <CardActions>
-                        <Button size="small">Batalhar</Button>
+                        <Button size="small" color="primary">Share</Button>
                     </CardActions>
                 </Card>
-                <Card sx={{ maxWidth: 400 }}>
-                    <CardMedia component="img" height="200" image="https://cdn.dribbble.com/users/817492/screenshots/5814123/media/5352ef6c514efdcdc213bdb676b4541a.png" />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">Pokédex</Typography>
-                        <Typography variant="body2" color="text.secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus maximus neque ut ipsum luctus facilisis. Quisque in facilisis augue. Morbi quis velit et dolor blandit condimentum.</Typography>
-                    </CardContent>
+                <Card sx={{ maxWidth: 520 }}>
+                    <CardActionArea>
+                        <CardMedia component="img" height="550" image="https://www.teahub.io/photos/full/95-957599_fanart-pokemon-sun-and-moon-and-mimikyu-image.gif" alt="green iguana" />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Lizard
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
                     <CardActions>
-                        <Button size="small">ACESSAR</Button>
+                        <Button size="small" color="primary">Share</Button>
                     </CardActions>
                 </Card>
-                <Card sx={{ maxWidth: 400 }}>
-                    <CardMedia component="img" height="200" image="https://i.pinimg.com/originals/89/49/e5/8949e5e697ca5d75b045080343c2c8c1.jpg" />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">Batalhas</Typography>
-                        <Typography variant="body2" color="text.secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus maximus neque ut ipsum luctus facilisis. Quisque in facilisis augue. Morbi quis velit et dolor blandit condimentum.</Typography>
-                    </CardContent>
+                <Card sx={{ maxWidth: 500 }}>
+                    <CardActionArea>
+                        <CardMedia component="img" height="550" image="https://i.pinimg.com/originals/ac/3c/04/ac3c048f9761eae7c58cb83800562af9.gif" alt="green iguana" />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Lizard
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
                     <CardActions>
-                        <Button size="small">Batalhar</Button>
-                    </CardActions>
-                </Card>
-                <Card sx={{ maxWidth: 400 }}>
-                    <CardMedia component="img" height="200" image="https://i.pinimg.com/originals/89/49/e5/8949e5e697ca5d75b045080343c2c8c1.jpg" />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">Batalhas</Typography>
-                        <Typography variant="body2" color="text.secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus maximus neque ut ipsum luctus facilisis. Quisque in facilisis augue. Morbi quis velit et dolor blandit condimentum.</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Batalhar</Button>
+                        <Button size="small" color="primary">Share</Button>
                     </CardActions>
                 </Card>
             </div>
